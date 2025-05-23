0da5552c89f9b20a642b78eee9f5d0a66c26e2fe
YanaTedeeva <yana.tedeeva.2005@mail.ru>
2025-05-23 11:48:06 +0300

    поменять чтобы вместо номеров были конкретные названия и чтобы жанры и публикаторы были в книгах

--- LibraryData/Models/Book.cs
+++ LibraryData/Models/Book.cs
@@ -5,12 +5,9 @@
         public int Id { get; set; }
         public string Title { get; set; }
         public int Year { get; set; }
-
-        // Внешний ключ на автора
         public int AuthorId { get; set; }
         public Author Author { get; set; }
 
-        // Навигационные свойства для связей многие-ко-многим
         public ICollection<BookGenre> BookGenres { get; set; }
         public ICollection<BookPublisher> BookPublishers { get; set; }
 
